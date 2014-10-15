795054e431d3d91ea31249daec7bbce6acac5b7d
John Paul <joaop.gomes@gmail.com>
2014-10-15 17:12:38 -0300

    ...

--- game.py
+++ game.py
@@ -31,46 +31,65 @@ class Game():
         self.player.walk_change_sprite(x_axis, y_axis)
 
         player_x, player_y = self.player.position[0]+x_axis, self.player.position[1]+y_axis
-
-        #x = player_x+((self.map.camera[0]))/32.0 * (-1)
-
-        #print player_x, self.map.camera[0], self.player.position[0]
         
         x = (self.map.camera[0] * (-1)) + self.player.position[0] if self.map.camera[0] < 0 else self.map.camera[0] + self.player.position[0]
         y = (self.map.camera[1] * (-1)) + self.player.position[1] if self.map.camera[1] < 0 else self.map.camera[1] + self.player.position[1]
         x = (x + 2) if x_axis > 0 else (x + 2)
         y = (y + 2) if y_axis > 0 else (y + 2)
-        #print x, y
-        #print int(ceil(x)), int(ceil(y))#x, int(ceil(x/32))
         
-        print int(ceil(y)/32), int(ceil(x)/32), self.map.tiles[int(ceil(y)/32)][int(ceil(x)/32)].can_walk
-        #if self.map.tiles[int(ceil((player_y+(7*y_axis)+(self.map.camera[1]*(-1)))/32))][int(ceil(x))].can_walk:
-        if self.map.move_camera(x_axis, y_axis):
-            self.map.set_camera(x_axis, y_axis)
-        else:
-            x_axis = x_axis*(-1)
-            y_axis = y_axis*(-1)
-            if x_axis != 0:
-                if x_axis > 0:
-                    if self.player.position[0] + x_axis > (800-25):
-                        x_axis = 0
-                else:
-                    if self.player.position[0] + x_axis + 25 < 25:
-                        x_axis = 0
-
+        y = int(ceil(y/32))
+        x = int(ceil(x/32))
+        
+        y_sum = 0
+        x_sum = 0
+        
+        if y_axis != 0:
+            y_sum = -1 if y_axis > 0 else 1
+        if x_axis != 0:
+            x_sum = -1 if x_axis > 0 else 1
+        x += x_sum
+        y += y_sum
+        
+        if x < 0:
+            x = 0
+        if x >= len(self.map.tiles[0]):
+            x = len(self.map.tiles[0]) - 1 
+        
+        if y < 0:
+            y = 0
+        if y > len(self.map.tiles):
+            y = len(self.map.tiles)
+        #print x, y, self.map.tiles[y][x].can_walk
+        if self.map.tiles[y][x].can_walk:
+            if self.map.move_camera(x_axis, y_axis):
+                self.map.set_camera(x_axis, y_axis)
+            else:
+                x_axis = x_axis*(-1)
+                y_axis = y_axis*(-1)
+                #print x_axis, y_axis
+                if x_axis != 0:
+                    if x_axis > 0:
+                        if self.player.position[0] + x_axis > (800-25):
+                            x_axis = 0
+                    else:
+                        if self.player.position[0] + x_axis + 25 < 25:
+                            x_axis = 0
+    
                 if y_axis != 0:
                     if y_axis > 0:
-                        if self.player.position[1] + y_axis > (608-38):
+                        print self.player.position[1] + y_axis
+                        if self.player.position[1] + y_axis > (608-38):                                
                             y_axis = 0
                     else:
                         if self.player.position[1] + y_axis + 38 < 38:
-                            y_axis = 0
+                            y_axis = 0                                
     
                 self.player.move_player(x_axis, y_axis)
     
     def main_loop(self):
         pygame.key.set_repeat(1, 10)
         while True:
+            
             for ev in event.get():
                 if ev.type == pygame.QUIT:
                     for i in range(30):
@@ -84,11 +103,11 @@ class Game():
                     self.player_input(pygame.key.get_pressed())            
             self.map.draw_map()
             self.player.draw()
+            self.player.make_move = False
             pygame.display.update()
-            self.time.tick(45)
+            self.time.tick(15)
 
 if __name__ == '__main__':
     pygame.init()
-    #pygame.image.load('images/map/grass.jpg')
     game = Game()
     game.main_loop()
--- player.py
+++ player.py
@@ -20,6 +20,7 @@ class Player():
         self.armor = None
         self.level = 1
         self.bag  = []
+        self.sprite_count = 0
         self.texture = dict()
         self.direction = 0
         self.position = [0, 0]
@@ -27,6 +28,7 @@ class Player():
         self.rect = {}
         self.image = {}
         self.load_player_sprite()
+        self.make_move = False
     
     def load_attributes(self):
         self.attr = PLAYER_LIST[self.name]
@@ -41,11 +43,18 @@ class Player():
         texture_manager.load_texture(self.name, 'images/character/'+self.name+'/'+skin+'/'+self.name+'.png')
         
         for x in range(4):
+            self.image[skin].append([])
+            self.rect[skin].append([])
+            
             for y in range(4):
-                self.image[skin].append(texture_manager.textures[self.name])
-                self.rect[skin].append(pygame.Rect(25.5*x, 38.25*y, 25.5, 38.25))
-    
+                self.image[skin][x].append(texture_manager.textures[self.name])
+                self.rect[skin][x].append(pygame.Rect(25.5*y, 38.25*x, 25.5, 38.25))
+                #self.image[skin].append(texture_manager.textures[self.name])
+                #self.rect[skin].append(pygame.Rect(25.5*x, 38.25*y, 25.5, 38.25))
+            
     def walk_change_sprite(self, x, y):
+        if x != 0 or y != 0:
+            self.make_move = True
         if x > 0:
             self.direction = 1
         elif x < 0:
@@ -55,9 +64,12 @@ class Player():
             self.direction = 3
         elif y < 0:
             self.direction = 0
+
     def move_player(self, x, y):
         self.position[0] += x
         self.position[1] += y
 
     def draw(self):
-        screen.blit(texture_manager.textures[self.name][0], (self.position[0], self.position[1]), self.rect['skin'][self.direction])
+        screen.blit(texture_manager.textures[self.name][0], (self.position[0], self.position[1]), self.rect['skin'][self.direction][self.sprite_count])
+        if self.make_move:
+            self.sprite_count = self.sprite_count+1 if self.sprite_count < 3 else 0
